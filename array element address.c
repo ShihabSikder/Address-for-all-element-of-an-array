567654abc79abd1183ff2a2b01a965a132b12475
ShihabSikder <38876495+ShihabSikder@users.noreply.github.com>
2018-05-01 12:18:21 +0600

    Add files via upload

---
+++ array element address.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+ int ary[3][3];
+ int x,y;
+
+  for(x=0;x<3;x++){
+     for(y=0;y<3;y++){
+        printf("Enter the Element [%d][%d] = ",x+1,y+1);
+        scanf("%d",&ary[x][y]);
+        printf("\n");
+     }
+    }
+ printf("\n Showing the inputed matrix \n");
+
+  for(x=0;x<3;x++){
+        printf("\n");
+     for(y=0;y<3;y++){
+       printf("\t %d \t",ary[x][y]);
+     }
+    }
+
+   printf("\n Now Showing the inputed matrix address for each element \n");
+
+  for(x=0;x<3;x++){
+        printf("\n");
+     for(y=0;y<3;y++){
+       printf("\t %d \t",&ary[x][y]);
+     }
+    }
+}
